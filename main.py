78b4cd9edaab5c96a69599011dbf825c3e6d6f83
TREZEV/TRX <92353919+trezev@users.noreply.github.com>
2024-01-06 21:42:44 +0600

    v1.3

--- main.py
+++ main.py
@@ -1,54 +1,66 @@
+from threading import Thread
 import requests
 import json
 import sys
 
-__version__ = "1.3beta03"
-
-with open("data.json", "r", encoding="utf-8") as f:
-    json_data = json.load(f)
+__version__ = "1.3"
 
 
 class Bloodtrail:
-    def __init__(self, phone, data):
-        if phone == "":
-            if data["last_used_number"]:
-                self.phone = data["last_used_number"]
+    def __init__(self, number):
+        with open("data.json", "r", encoding="utf-8") as f:
+            json_data = json.load(f)
+        self.data = json_data
+        if number == "":
+            if self.data["last_used_number"]:
+                self.number = self.data["last_used_number"]
+                print(f"{'USING LATEST NUMBER :': >40} {self.data['last_used_number']}")
                 pass
             else:
                 print(f"{'ERROR :': >40} write number as a target to save it")
                 sys.exit()
         else:
-            self.phone = phone
-            data["last_used_number"] = self.phone
+            self.number = number
+            self.data["last_used_number"] = self.number
             with open("data.json", "w", encoding="utf-8") as j:
-                json.dump(obj=data, fp=j, ensure_ascii=False, indent=4)
-        self.data = data
+                json.dump(obj=self.data, fp=j, ensure_ascii=False, indent=4)
 
-    @staticmethod
-    def print_request_result(r_status_code, r_service_name):
-        print(f"{'REQUEST STATUS OF ' + r_service_name + ' :': >40} {r_status_code}")
-
-    def format_data_in_services(self):
+    def format_data(self):
         for i in self.data["services"]:
             for k, v in self.data["services"][i]["data"].items():
-                if "%PHONE%" in v:
-                    v = v.replace("%PHONE%", self.phone[int(self.data["services"][i]["phone_f"]):])
+                if "%NUMBER%" in v:
+                    v = v.replace("%NUMBER%", self.number[int(self.data["services"][i]["phone_f"]):])
                     self.data["services"][i]["data"][k] = v
 
-    def send_requests(self, repeats=1):
+    @staticmethod
+    def post_request(service, _url, _json=None, _data=None):
+        if _json:
+            r = requests.post(url=_url,
+                              json=_json)
+        else:
+            r = requests.post(url=_url,
+                              data=_data)
+        return print(f"{'REQUEST STATUS OF ' + service + ' :': >40} {r.status_code}")
+
+    def start_threads(self, repeats=1):
         for i in range(repeats):
-            for s in self.data["services"]:
-                r_type = self.data["services"][s]["data_type"]
-                if r_type == "json":
-                    r = (requests.post(url=self.data["services"][s]["url"],
-                                       json=self.data["services"][s]["data"]))
+            for service in self.data["services"]:
+                data_type = self.data["services"][service]["data_type"]
+                if data_type == "json":
+                    t = Thread(target=self.post_request, args=(service,
+                                                               self.data["services"][service]["url"],
+                                                               self.data["services"][service]["data"],
+                                                               None))
+                    t.start()
                 else:
-                    r = (requests.post(url=self.data["services"][s]["url"],
-                                       data=self.data["services"][s]["data"]))
-                self.print_request_result(r.status_code, s)
+                    t = Thread(target=self.post_request, args=(service,
+                                                               self.data["services"][service]["url"],
+                                                               None,
+                                                               self.data["services"][service]["data"]))
+                    t.start()
 
 
 if __name__ == '__main__':
-    script = Bloodtrail(input(f"{'BLOODTRAIL :': >40} {__version__}\n{'TARGET NUMBER :': >40} "), json_data)
-    script.format_data_in_services()
-    script.send_requests(int(input(f"{'REPEATS COUNT :': >40} ")))
+    bloodtrail = Bloodtrail(input(f"{'BLOODTRAIL :': >40} {__version__}\n{'TARGET NUMBER :': >40} "))
+    bloodtrail.format_data()
+    bloodtrail.start_threads((int(input(f"{'REPEATS COUNT :': >40} "))))
